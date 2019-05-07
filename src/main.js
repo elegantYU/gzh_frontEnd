627c7af07f3fffe45404ee4dc7b76e6855b2b9af
elegantYU <845384559@qq.com>
2019-05-07 23:49:18 +0800

    首页

--- src/main.js
+++ src/main.js
@@ -1,13 +1,17 @@
 import Vue from 'vue'
 import Axios from 'axios'
 import VueAxios from 'vue-axios'
+import { Swipe, SwipeItem } from 'mint-ui'
+import 'mint-ui/lib/style.css'
 import App from './App.vue'
 import router from './router/router'
 import store from './store/store'
-import baseCss from './assets/css/base.css'
+import './assets/css/base.css'
 import { deviceRem } from './utils/utils'
 
 Vue.use(VueAxios, Axios)
+Vue.component(Swipe.name, Swipe)
+Vue.component(SwipeItem.name, SwipeItem)
 
 Vue.config.productionTip = false
 
--- src/views/index/Index.vue
+++ src/views/index/Index.vue
@@ -1,6 +1,114 @@
 <template>
   <div class="index_wrapper">
-    这是首页
+    <div class="index_swipe">
+      <span class="index_map"><i></i>康馨园</span>
+      <span class="index_notice"></span>
+      <mt-swipe>
+        <mt-swipe-item>1</mt-swipe-item>
+        <mt-swipe-item>2</mt-swipe-item>
+        <mt-swipe-item>3</mt-swipe-item>
+      </mt-swipe>
+    </div>
+    <div class="index_tools">
+      <h5>智慧小区</h5>
+      <ul class="index_tools_panel clearfix">
+        <div>
+          <li
+            v-for="(v, i) in v_tools1"
+            :key="i"
+            @click="f_moveto(v)"
+          >
+            <img :src="v.icon" alt="">
+            <span>{{ v.msg }}</span>
+          </li>
+        </div>
+        <div>
+          <li
+            v-for="(v, i) in v_tools2"
+            :key="i"
+            @click="f_moveto(v)"
+          >
+            <img :src="v.icon" alt="">
+            <span>{{ v.msg }}</span>
+          </li>
+        </div>
+      </ul>
+    </div>
+    <div class="index_topic">
+      <h5>小区头条<span>更多<i></i></span></h5>
+      <ul class="index_topic_item">
+        <li>
+          <div class="index_topic_item_left">
+            <p>这还是打好啦收到货拉三打哈是理科地哈萨克老大和爱上了DHLask接阿斯大三大四的撒多撒多电话静安寺</p>
+            <div class="index_topic_item_detail clearfix">
+              <span>来源 通知通告</span>
+              <ul class="clearfix">
+                <li><i></i>320</li>
+                <li><i></i>320</li>
+                <li><i></i>320</li>
+              </ul>
+            </div>
+          </div>
+          <img src="https://wallpapers.wallhaven.cc/wallpapers/full/wallhaven-94316.jpg" alt="">
+        </li>
+        <li>
+          <div class="index_topic_item_left">
+            <p>这还是打好啦收到货拉三打哈是理科地哈萨克老大和爱上了DHLask接阿斯大三大四的撒多撒多电话静安寺</p>
+            <div class="index_topic_item_detail clearfix">
+              <span>来源 通知通告</span>
+              <ul class="clearfix">
+                <li><i></i>320</li>
+                <li><i></i>320</li>
+                <li><i></i>320</li>
+              </ul>
+            </div>
+          </div>
+          <img src="https://wallpapers.wallhaven.cc/wallpapers/full/wallhaven-94316.jpg" alt="">
+        </li>
+        <li>
+          <div class="index_topic_item_left">
+            <p>这还是打好啦收到货拉三打哈是理科地哈萨克老大和爱上了DHLask接阿斯大三大四的撒多撒多电话静安寺</p>
+            <div class="index_topic_item_detail clearfix">
+              <span>来源 通知通告</span>
+              <ul class="clearfix">
+                <li><i></i>320</li>
+                <li><i></i>320</li>
+                <li><i></i>320</li>
+              </ul>
+            </div>
+          </div>
+          <img src="https://wallpapers.wallhaven.cc/wallpapers/full/wallhaven-94316.jpg" alt="">
+        </li>
+        <li>
+          <div class="index_topic_item_left">
+            <p>这还是打好啦收到货拉三打哈是理科地哈萨克老大和爱上了DHLask接阿斯大三大四的撒多撒多电话静安寺</p>
+            <div class="index_topic_item_detail clearfix">
+              <span>来源 通知通告</span>
+              <ul class="clearfix">
+                <li><i></i>320</li>
+                <li><i></i>320</li>
+                <li><i></i>320</li>
+              </ul>
+            </div>
+          </div>
+          <img src="https://wallpapers.wallhaven.cc/wallpapers/full/wallhaven-94316.jpg" alt="">
+        </li>
+        <li>
+          <div class="index_topic_item_left">
+            <p>这还是打好啦收到货拉三打哈是理科地哈萨克老大和爱上了DHLask接阿斯大三大四的撒多撒多电话静安寺</p>
+            <div class="index_topic_item_detail clearfix">
+              <span>来源 通知通告</span>
+              <ul class="clearfix">
+                <li><i></i>320</li>
+                <li><i></i>320</li>
+                <li><i></i>320</li>
+              </ul>
+            </div>
+          </div>
+          <img src="https://wallpapers.wallhaven.cc/wallpapers/full/wallhaven-94316.jpg" alt="">
+        </li>
+      </ul>
+    </div>
     <Tabs></Tabs>
   </div>
 </template>
@@ -10,12 +118,245 @@ import Tabs from '../../components/Tab.vue'
 
 export default {
   name: 'Index',
+  data () {
+    return {
+      v_tools1: [
+        { msg: '物业报修', path: '', icon: require('../../assets/images/index/index_wy.png') },
+        { msg: '访客通行', path: '', icon: require('../../assets/images/index/index_fk.png') },
+        { msg: '预约服务', path: '', icon: require('../../assets/images/index/index_fw.png') },
+        { msg: '失物招领', path: '', icon: require('../../assets/images/index/index_sw.png') },
+      ],
+      v_tools2: [
+        { msg: '智慧停车', path: '', icon: require('../../assets/images/index/index_zh.png') },
+        { msg: '生活超市', path: '', icon: require('../../assets/images/index/index_sh.png') },
+        { msg: '邻里共享', path: '', icon: require('../../assets/images/index/index_ll.png') },
+        { msg: '全部', path: '', icon: require('../../assets/images/index/index_qb.png') }
+      ],
+    }
+  },
   components: {
     Tabs
+  },
+  methods: {
+    f_moveto (v) {
+      console.log(v.msg)
+    }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-
+.index_wrapper{
+  padding-bottom: 1.2rem;
+  background-color: #efeff4;
+  .index_swipe{
+    height: 3.1rem;
+    position: relative;
+    .mint-swipe{
+      height: 100%;
+      .mint-swipe-item{
+        background-color: blue;
+      }
+      .mint-swipe-indicator{
+        background-color: #b3b3b3;
+        &.is-active{
+          background-color: #fff;
+        }
+      }
+    }
+    .index_map{
+      position: absolute;
+      top: 0.1rem;
+      left: 0.3rem;
+      width: auto;
+      height: 0.21rem;
+      z-index: 1;
+      font-size: 0.18rem;
+      line-height: 0.21rem;
+      color: #fff;
+      i{
+        display: inline-block;
+        width: 0.16rem;
+        height: 0.21rem;
+        background-image: url('../../assets/images/index/index_map.png');
+        background-repeat: no-repeat;
+        background-position: center center;
+        background-size: contain;
+        margin-right: 0.1rem;
+        vertical-align: middle;
+      }
+    }
+    .index_notice{
+      position: absolute;
+      top: 0.1rem;
+      right: 0.3rem;
+      width: 0.23rem;
+      height: 0.26rem;
+      z-index: 1;
+      background-image: url('../../assets/images/index/index_notice.png');
+      background-repeat: no-repeat;
+      background-size: contain;
+      background-position: center center;
+      cursor: pointer;
+      &.index_noticed{
+        background-image: url('../../assets/images/index/index_noticed.png');
+      }
+    }
+  }
+  .index_tools{
+    padding: 0.2rem 0.3rem;
+    h5{
+      font-size: 0.3rem;
+      font-weight: bold;
+      margin-bottom: 0.15rem;
+      text-align: left;
+    }
+    .index_tools_panel{
+      padding: 0.32rem 0.28rem 0.22rem;
+      background-color: #fff;
+      border-radius: 0.05rem;
+      box-shadow: 0 4px 10px rgba(124, 134, 138, 0.2);
+      div{
+        display: flex;
+        justify-content: space-between;
+        li{
+          width: 1.2rem;
+          margin-bottom: 0.33rem;
+          text-align: center;
+          cursor: pointer;
+          img{
+            width: 0.88rem;
+            height: 0.82rem;
+            margin-bottom: 0.22rem;
+          }
+          span{
+            display: block;
+            font-size: 0.22rem;
+          }
+        }
+      }
+    }
+  }
+  .index_topic{
+    padding: 0.2rem 0.3rem;
+    h5{
+      font-size: 0.3rem;
+      font-weight: bold;
+      margin-bottom: 0.15rem;
+      text-align: left;
+      span{
+        float: right;
+        color: #999;
+        font-size: 0.24rem;
+        font-weight: normal;
+        cursor: pointer;
+        i{
+          display: inline-block;
+          width: 0.1rem;
+          height: 0.2rem;
+          background-image: url('../../assets/images/index/index_more.png');
+          background-repeat: no-repeat;
+          background-size: contain;
+          background-position: center center;
+          margin-left: 0.1rem;
+        }
+      }
+    }
+    .index_topic_item{
+      &>li{
+        height: 1.5rem;
+        padding: 0.2rem;
+        padding-right: 0.1rem;
+        background-color: #fff;
+        box-sizing: border-box;
+        border-left: 0.03rem solid #ff8d3a;
+        border-radius: 0.05rem;
+        box-shadow: 0 4px 10px rgba(124, 134, 138, 0.2);
+        margin-bottom: 0.16rem;
+        .index_topic_item_left{
+          display: inline-block;
+          width: calc(100% - 1.7rem);
+          padding-right: 0.15rem;
+          box-sizing: border-box;
+          p{
+            font-size: 0.26rem;
+            text-align: left;
+            margin-bottom: 0.35rem;
+            text-overflow: -o-ellipsis-lastline;
+            text-overflow: ellipsis;
+            overflow: hidden;
+            display: -webkit-box;
+            -webkit-line-clamp: 2;
+            /*! autoprefixer: off */
+            -webkit-box-orient: vertical;
+          }
+          .index_topic_item_detail{
+            font-size: 0.18rem;
+            color: #888;
+            span{
+              float: left;
+            }
+            ul{
+              float: right;
+              li{
+                float: left;
+                font-size: 0.2rem;
+                color: #999;
+                margin-right: 0.16rem;
+                i{
+                  display: inline-block;
+                  width: 0.24rem;
+                  height: 0.22rem;
+                  background-repeat: no-repeat;
+                  background-size: contain;
+                  background-position: center center;
+                  vertical-align: middle;
+                  margin-right: 0.05rem;
+                }
+                &:nth-of-type(1){
+                  i{
+                    background-image: url('../../assets/images/index/index_view.png');
+                  }
+                  &.index_topic_icon_active{
+                    color: #58c3fb;
+                    i{
+                      background-image: url('../../assets/images/index/index_viewed.png');
+                    }
+                  }
+                }
+                &:nth-of-type(2){
+                  i{
+                    background-image: url('../../assets/images/index/index_comment.png');
+                  }
+                  &.index_topic_icon_active{
+                    color: #fbbc58;
+                    i{
+                      background-image: url('../../assets/images/index/index_commented.png');
+                    }
+                  }
+                }
+                &:nth-of-type(3){
+                  i{
+                    background-image: url('../../assets/images/index/index_collect.png');
+                  }
+                  &.index_topic_icon_active{
+                    color: #ff6c73;
+                    i{
+                      background-image: url('../../assets/images/index/index_collected.png');
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+        img{
+          display: inline-block;
+          width: 1.7rem;
+          height: 100%;
+        }
+      }
+    }
+  }
+}
 </style>

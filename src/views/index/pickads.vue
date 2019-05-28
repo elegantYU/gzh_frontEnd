357598b238f9585595f4edeb1ea36723437076cd
lisa <82384780@qq.com>
2019-05-28 22:25:53 +0800

    update pickads

--- src/views/index/pickads.vue
+++ src/views/index/pickads.vue
@@ -1,12 +1,16 @@
 <template>
-  <div>
+  <div class="wp">
+    <div class="search">
+      <input type="text" placeholder="输入小区名称进行搜索">
+      <img class="serch-icon" :src="require('@/assets/images/pickads/search.png')" alt="">
+    </div>
     <div class="pick-wp">
       <div class="left">
-        <div v-for="v in left"
+        <div class="fa" v-for="v in left"
         :key="v.id">{{v}}</div>
       </div>
       <div class="rigth">
-        <div v-for="v in right"
+        <div class="child" v-for="v in right"
         :key="v.id">{{v.name}}</div>
       </div>
     </div>
@@ -51,6 +55,9 @@ export default {
             console.log('市', res)
             if (res.data.success) {
               this.right = res.data.data
+              for(var i = 0; i<this.right.length -1; i++) {
+                this.left.push(' ')
+              }
             }
           })
       })
@@ -62,15 +69,61 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
+.wp {
+  background: #efeff4;
+  width: 100%;
+  height: 100%;
+  padding-top: 0.18rem;
+}
 .pick-wp {
   display: flex;
   .left{
-    width: 1rem;
+    width: 2.4rem;
+    .fa {
+      height: 0.88rem;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      background: #eaeaea;
+      border-bottom: 0.01rem solid #c2c2c2;
+      &:nth-of-type(1) {
+        border-top: 0.01rem solid #c2c2c2;
+      }
+    }
   }
   .rigth {
     flex: 1;
-    // display: flex;
-    // flex-direction: column;
+    .child {
+      height: 0.88rem;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      background: #fff;
+      &:nth-of-type(1) {
+        border-top: 0.01rem solid #c2c2c2;
+      }
+      border-bottom: 0.01rem solid #c2c2c2;
+    }
+  }
+}
+.search {
+  position: relative;
+  width: 6.9rem;
+  margin: 0 auto 0.18rem;
+  input {
+    text-indent: 1rem;
+    width: 6.9rem;
+    height: 0.6rem;
+    line-height: 0.6rem;
+    background: #fff;
+    border-radius: 0.05rem;
+    font-size: 0.2rem;
+  }
+  .serch-icon {
+    width: 0.33rem;
+    position: absolute;
+    left: 0.29rem;
+    top: 0.15rem;
   }
 }
 </style>

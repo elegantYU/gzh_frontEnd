1eb7f1f279eb4ffe6ccf59fb6703bd7b6bcfabf7
lisa <82384780@qq.com>
2019-06-10 22:53:08 +0800

    update pickads

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -23,6 +23,6 @@ Axios.interceptors.response.use(res => {
   return res
 })
 
-Axios.defaults.baseURL = 'http://wx.feng360.com:9999'
+// Axios.defaults.baseURL = 'http://wx.feng360.com:9999'
 
 export default Axios
